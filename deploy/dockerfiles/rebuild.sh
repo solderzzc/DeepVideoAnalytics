afc72191442a36ba2888d0cfee0b9a0c73f7cf56
Simba Zhang <solderzzc@gmail.com>
2018-01-16 10:48:50 -0800

    Switch base repo to this one.

--- deploy/dockerfiles/rebuild.sh
+++ deploy/dockerfiles/rebuild.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -xe
-docker build -t akshayubhat/dva-auto:latest .
-docker build -t akshayubhat/dva-auto:gpu -f Dockerfile.gpu .
+docker build -t solderzzc/dva-auto:latest .
+#docker build -t akshayubhat/dva-auto:gpu -f Dockerfile.gpu .
